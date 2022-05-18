755a34fe5ddff8f249c3e13e9495b54250a90f37
Yoeri Boven <yoeriboven@msn.com>
2022-05-18 16:20:16 +0200

    Removed factory

--- src/Models/LogMessage.php
+++ src/Models/LogMessage.php
@@ -3,12 +3,10 @@
 namespace Yoeriboven\LaravelLogDb\Models;
 
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LogMessage extends Model
 {
-    use HasFactory;
 
     /**
      * Indicates if the model should be timestamped.
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,11 @@
 
 namespace Yoeriboven\LaravelLogDb\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Yoeriboven\LaravelLogDb\DatabaseLoggerServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Yoeriboven\\LaravelLogDb\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
