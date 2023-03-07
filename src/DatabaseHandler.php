0608a4f150eb0ff77d5bd1c3ba0f9f7a5afb6cea
Yoeri Boven <yoeriboven@msn.com>
2023-03-07 21:41:17 +0100

    Exceptions produce an empty object in the log (#10)
    * fixed bug that left context empty if an exception was thrown
    
    * added laravel 10 automated testing
    
    * wip

--- src/DatabaseHandler.php
+++ src/DatabaseHandler.php
@@ -3,6 +3,7 @@
 namespace Yoeriboven\LaravelLogDb;
 
 use Monolog\Handler\AbstractProcessingHandler;
+use Throwable;
 use Yoeriboven\LaravelLogDb\Models\LogMessage;
 
 class DatabaseHandler extends AbstractProcessingHandler
@@ -12,6 +13,14 @@ class DatabaseHandler extends AbstractProcessingHandler
      */
     protected function write($record): void
     {
+        $record = is_array($record) ? $record : $record->toArray();
+
+        $exception = $record['context']['exception'] ?? null;
+
+        if ($exception instanceof Throwable) {
+            $record['context']['exception'] = (string) $exception;
+        }
+
         LogMessage::create([
             'level' => $record['level'],
             'level_name' => $record['level_name'],
