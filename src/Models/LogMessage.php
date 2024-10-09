31df406f1e46b3adc13d6f056f64722430c7bbdc
Yoeri Boven <yoeriboven@msn.com>
2024-10-09 20:08:45 +0200

    wip

--- src/Models/LogMessage.php
+++ src/Models/LogMessage.php
@@ -3,10 +3,13 @@
 namespace Yoeriboven\LaravelLogDb\Models;
 
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 
 class LogMessage extends Model
 {
+    use MassPrunable;
+
     /**
      * The attributes that aren't mass assignable.
      *
@@ -38,5 +41,15 @@ class LogMessage extends Model
         $connection = config('logging.channels.db.connection') ?? config('database.default');
 
         $this->setConnection($connection);
+    }
+
+    /**
+     * Get the prunable model query.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function prunable()
+    {
+        return static::where('logged_at', '<=', now()->subDays(config('logging.channels.db.days')));
     }
 }
